c6d58a64b2b4da186fa82de17a6c6afa252bfe45
Ronny Hildebrandt <ronny.hildebrandt@avorium.de>
2021-01-23 22:04:26 +0100

    Was vergessen

--- force-app/main/default/lwc/questlistitem/questlistitem.js
+++ force-app/main/default/lwc/questlistitem/questlistitem.js
@@ -12,6 +12,9 @@ export default class Questlistitem extends LightningElement {
 
     handleCheckButtonClick() {
         this.checked = !this.checked;
+
+        // TODO: Update Record: https://developer.salesforce.com/docs/component-library/documentation/en/lwc/lwc.reference_update_record
+        
     }
       
 }
