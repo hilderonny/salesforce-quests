797c967e60fdd2237eddf92d777b0e6f1935759a
Ronny Hildebrandt <ronny.hildebrandt@avorium.de>
2021-01-23 23:54:15 +0100

    Quests lassen sich abhaken, LIAGA eingebaut

--- force-app/main/default/lwc/questlistitem/questlistitem.js
+++ force-app/main/default/lwc/questlistitem/questlistitem.js
@@ -1,4 +1,8 @@
 import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { updateRecord } from 'lightning/uiRecordApi';
+import COMPLETE_FIELD from '@salesforce/schema/Quest__c.Complete__c';
+import ID_FIELD from '@salesforce/schema/Quest__c.Id';
 
 // Items: https://lightningdesignsystem.com/icons/
 
@@ -10,10 +14,40 @@ export default class Questlistitem extends LightningElement {
     @api
     label = '';
 
+    @api
+    recordid = '';
+
     handleCheckButtonClick() {
         this.checked = !this.checked;
 
         // TODO: Update Record: https://developer.salesforce.com/docs/component-library/documentation/en/lwc/lwc.reference_update_record
+
+        const fields = {};
+        fields[ID_FIELD.fieldApiName] = this.recordid;
+        fields[COMPLETE_FIELD.fieldApiName] = this.checked;
+
+        const recordInput = { fields };
+
+        console.log(recordInput);
+
+        updateRecord(recordInput).then(() => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Quest updated',
+                    variant: 'success'
+                })
+            );
+        })
+        .catch(error => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error updating quest',
+                    message: error.body.message,
+                    variant: 'error'
+                })
+            );
+        });
         
     }
       
