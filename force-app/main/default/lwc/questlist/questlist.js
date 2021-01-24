ce1bcfae94fef47fc8fd7aac2c1c985c4d6fc55f
Ronny Hildebrandt <ronny.hildebrandt@avorium.de>
2021-01-24 21:37:30 +0100

    Questliste verbessert

--- force-app/main/default/lwc/questlist/questlist.js
+++ force-app/main/default/lwc/questlist/questlist.js
@@ -1,9 +1,40 @@
 import { LightningElement, wire } from 'lwc';
 import getAllQuests from '@salesforce/apex/QuestController.getAllQuests';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { updateRecord } from 'lightning/uiRecordApi';
 
 export default class Questlist extends LightningElement {
     
     @wire(getAllQuests)
     quests;
+
+    handleTileCompleteClick(event) {
+        const quest = event.detail;
+        const tile = event.target;
+        quest.Complete__c = !quest.Complete__c;
+        const record = { fields: {
+            Id: quest.Id,
+            Complete__c: quest.Complete__c
+        } };
+        updateRecord(record).then(() => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Quest updated',
+                    variant: 'success'
+                })
+            );
+            tile.quest = quest; // Trigger re-render of tile
+        })
+        .catch(error => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error updating quest',
+                    message: error.body.message,
+                    variant: 'error'
+                })
+            );
+        });
+    }
 
 }
--- force-app/main/default/lwc/questlistitem/questlistitem.js
+++ force-app/main/default/lwc/questlistitem/questlistitem.js
@@ -1,53 +1,16 @@
 import { LightningElement, api } from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import { updateRecord } from 'lightning/uiRecordApi';
-import COMPLETE_FIELD from '@salesforce/schema/Quest__c.Complete__c';
-import ID_FIELD from '@salesforce/schema/Quest__c.Id';
 
-// Items: https://lightningdesignsystem.com/icons/
+// Icons: https://lightningdesignsystem.com/icons/
 
 export default class Questlistitem extends LightningElement {
 
     @api
-    checked = false;
+    quest = '';
 
-    @api
-    label = '';
+    handleCompleteButtonClick() {
 
-    @api
-    recordid = '';
-
-    handleCheckButtonClick() {
-        this.checked = !this.checked;
-
-        // TODO: Update Record: https://developer.salesforce.com/docs/component-library/documentation/en/lwc/lwc.reference_update_record
-
-        const fields = {};
-        fields[ID_FIELD.fieldApiName] = this.recordid;
-        fields[COMPLETE_FIELD.fieldApiName] = this.checked;
-
-        const recordInput = { fields };
-
-        console.log(recordInput);
-
-        updateRecord(recordInput).then(() => {
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'Success',
-                    message: 'Quest updated',
-                    variant: 'success'
-                })
-            );
-        })
-        .catch(error => {
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'Error updating quest',
-                    message: error.body.message,
-                    variant: 'error'
-                })
-            );
-        });
+        const event = new CustomEvent('completeclick', { detail: this.quest });
+        this.dispatchEvent(event);
         
     }
       
