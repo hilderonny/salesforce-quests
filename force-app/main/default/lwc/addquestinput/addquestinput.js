d73ee72b33c72d3ebcc70b3d451af860523c424d
hilderonny <hilderonny2014@gmail.com>
2021-05-11 10:17:32 +0200

    Die Standard-APIs funktionieren nicht richtig.

---
+++ force-app/main/default/lwc/addquestinput/addquestinput.js
@@ -0,0 +1,30 @@
+import { LightningElement } from 'lwc';
+import { createRecord } from 'lightning/uiRecordApi'; // https://developer.salesforce.com/docs/component-library/documentation/en/lwc/lwc.data_salesforce_write
+import QUEST_OBJECT from '@salesforce/schema/Quest__c';
+import NAME_FIELD from '@salesforce/schema/Quest__c.Name';
+import COMPLETE_FIELD from '@salesforce/schema/Quest__c.Complete__c';
+
+export default class Addquestinput extends LightningElement {
+
+    // Handle pressing enter in input field
+    handleCommit() {
+        // Fetch entered quest name
+        const input = this.template.querySelector('lightning-input');
+        const name = input.value;
+
+        // Create Quest
+        const fields = {};
+        fields[NAME_FIELD.fieldApiName] = name;
+        fields[COMPLETE_FIELD.fieldApiName] = false;
+        const recordInput = { apiName: QUEST_OBJECT.objectApiName, fields };
+        createRecord(recordInput).then(quest => {
+            console.log(quest);
+            // Empty input field
+            input.value = '';
+            // Inform list
+            const event = new CustomEvent('questcreated', { detail: quest });
+            this.dispatchEvent(event);
+        });
+    }
+
+}
--- force-app/main/default/lwc/questlist/questlist.js
+++ force-app/main/default/lwc/questlist/questlist.js
@@ -1,14 +1,31 @@
 import { LightningElement, wire } from 'lwc';
-import getAllQuests from '@salesforce/apex/QuestController.getAllQuests';
+import { getListUi } from 'lightning/uiListApi'; // https://developer.salesforce.com/docs/component-library/documentation/en/lwc/lwc.reference_get_list_ui
+import QUEST_OBJECT from '@salesforce/schema/Quest__c';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { updateRecord } from 'lightning/uiRecordApi';
+import { refreshApex } from '@salesforce/apex';
 
 export default class Questlist extends LightningElement {
-    
-    @wire(getAllQuests)
+
     quests;
+    questsData;
+    
+    @wire(getListUi, { objectApiName: QUEST_OBJECT, listViewApiName: 'All' })
+    questListView({ error, data }) {
+        console.log(error, data);
+        if (data) {
+            this.questsData = data.records;
+            this.quests = data.records.records;
+        }
+    };
+
+    handleQuestCreated(event) {
+        console.log(event);
+        this.quests.push(event.detail);
+    }
 
     handleTileCompleteClick(event) {
+        /*
         const quest = event.detail;
         const tile = event.target;
         quest.Complete__c = !quest.Complete__c;
@@ -35,6 +52,7 @@ export default class Questlist extends LightningElement {
                 })
             );
         });
+        */
     }
 
 }
